8b90aadb3b37316e1dd4831aea954fa3f19267cc
Mounish-R <rmounish44@gmail.com>
2026-01-12 01:35:25 +0530

    Add build-time env logging

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
 };
+
+console.log("--- BUILD TIME ENV CHECK ---");
+console.log("API_KEY present:", !!process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+console.log("Full Env Keys:", Object.keys(process.env).filter(k => k.startsWith('NEXT_')));
+console.log("----------------------------");
 
 export default nextConfig;
